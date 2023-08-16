6a0449baa85b68934fe8de4c34e68083eac48e23
mh3r31799 <mh3r31799@example.com>
2023-08-16 16:13:36 +1000

    -

--- advent2020/src/day8.py
+++ advent2020/src/day8.py
@@ -13,7 +13,7 @@ def part2():
     pass
 
 
-filename = "..\\data\\d6_input.txt"
+filename = "..\\data\\d8_input.txt"
 # filename = "..\\data\\test.txt"
 abs_file_path = os.path.join(os.path.dirname(__file__), filename)
 lines = open(abs_file_path, "r").readlines()
