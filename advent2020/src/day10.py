9dee040a6aee07f9da3acdb58109ae71bde9f345
mh3r31799 <mh3r31799@example.com>
2023-08-18 12:20:30 +1000

    copier

--- advent2020/src/day10.py
+++ advent2020/src/day10.py
@@ -19,12 +19,14 @@ def part2():
     pass
 
 
-filename = "..\\data\\dx_input.txt"
+filename = "..\\data\\d10_input.txt"
 switchToTest()
 
 abs_file_path = os.path.join(os.path.dirname(__file__), filename)
 lines = open(abs_file_path, "r").readlines()
 lines = list(map(lambda x: x.strip(), lines))
+
+print (lines)
 
 part1()
 part2()
