d5757d93760ad578ede95ac20a4cc46033cd8730
mh3r <96151246+mh3r@users.noreply.github.com>
2023-12-01 16:30:28 +1100

    1, 1.5

---
+++ advent2023/src/day1.py
@@ -0,0 +1,71 @@
+# from parse import *
+from functools import reduce
+import util
+import re
+import json
+import os
+import types
+import math
+
+
+def switchToTest():
+    global filename
+    filename = "..\\data\\test.txt"
+
+
+def part1(lines):
+    counts = []
+    for line in lines:
+        print(line)
+        numbers = re.findall(r"\d", line)
+        counts.append(int(numbers[0] + numbers[-1]))
+    pass
+
+    print(counts)
+    ans = sum(counts)
+    print("total ", ans)
+
+
+def part2(lines):
+    counts = []
+    for line in lines:
+        line = line.replace("oneight", "18")
+        line = line.replace("twone", "21")
+        line = line.replace("threeight", "38")
+        line = line.replace("fiveight", "58")
+        line = line.replace("sevenine", "79")
+        line = line.replace("eighthree", "83")
+        line = line.replace("eightwo", "82")
+        line = line.replace("nineight", "98")
+
+        line = line.replace("one", "1")
+        line = line.replace("two", "2")
+        line = line.replace("three", "3")
+        line = line.replace("four", "4")
+        line = line.replace("five", "5")
+        line = line.replace("six", "6")
+        line = line.replace("seven", "7")
+        line = line.replace("eight", "8")
+        line = line.replace("nine", "9")
+        print(line)
+        numbers = re.findall(r"\d", line)
+        counts.append(int(numbers[0] + numbers[-1]))
+    pass
+
+    print(counts)
+    ans = sum(counts)
+    print("total ", ans)
+    pass
+
+
+filename = "..\\data\\d1_input.txt"
+# switchToTest()
+
+abs_file_path = os.path.join(os.path.dirname(__file__), filename)
+lines = open(abs_file_path, "r").readlines()
+lines = list(map(lambda x: x.strip(), lines))
+
+print(*lines, sep="\n")
+
+# part1(lines)
+part2(lines)
---
+++ advent2023/src/util.py
@@ -0,0 +1,16 @@
+# from email.errors import FirstHeaderLineIsContinuationDefect
+# import parse
+
+
+# def parseToList(template, input):
+#     return list(parse.parse(template, input))
+
+
+ADJ_DIRS = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+ADJ_DIRS_2 = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [-1, 1], [-1, -1], [1, -1]]
+
+
+def reverseString(input):
+    retval = input[::-1] if not None else input
+    return retval
