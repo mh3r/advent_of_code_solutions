4059b8f0206c32f91a119ac839f210e3f6185215
mh3r31799 <mh3r31799@example.com>
2023-12-05 17:17:30 +1100

    day 5.1

---
+++ advent2023/src/day5.py
@@ -0,0 +1,111 @@
+# from parse import *
+from functools import reduce
+import util
+import re
+import json
+import os
+import types
+import math
+
+
+def switchToTest():
+    global filename
+    filename = "..\\data\\test.txt"
+
+
+def init(lines):
+    global seeds
+    global mapRules
+    tmpArray = []
+    mapName = None
+    print(len(lines))
+    counter = 1
+    for line in lines:
+        if "seeds" in line:
+            seeds = line.split(":")[1].strip().split(" ")
+            counter += 1
+            continue
+        if "map:" in line or counter == len(lines):
+            if mapName is not None:
+                mapRules[mapName] = tmpArray
+                tmpArray = []
+
+            mapName = line.split(" ")[0]
+            counter += 1
+            continue
+        if line.strip():
+            splitted = line.split(" ")
+            tmpArray.append([int(splitted[0]), int(splitted[1]), int(splitted[2])])
+        counter += 1
+    seeds = list(map(lambda x: int(x), seeds))
+
+
+def part1():
+    global seeds, mapRules
+
+    source = "seed"
+    dests = []
+    for index in seeds:
+        destination = nextDestination(source)
+        while destination is not None:
+            if destination == "fertilizer-to-water":
+                i = 4
+            index = findNewLocation(index, mapRules[destination])
+            destination = nextDestination(destination)
+            print(index)
+        dests.append(index)
+
+    print("answer", min(dests))
+
+    # newLocation = findNewLocation(81, mapRules["soil-to-fertilizer"])
+    # print(newLocation)
+
+
+def part2():
+    pass
+
+
+def findNewLocation(index, locationList):
+    retval = index
+    locationFound = None
+    for location in locationList:
+        start, end, theRange = location
+        if index >= end and index < end + theRange:
+            locationFound = location
+        # print(start, end, theRange)
+    if locationFound is not None:
+        start, end, theRange = locationFound
+        retval = index - end + start
+
+    return retval
+
+
+def nextDestination(previousMap):
+    global mapRules
+    if "-" not in previousMap:
+        return "seed-to-soil"
+    source = previousMap.split("-")[2]
+    for key in mapRules:
+        if source + "-" in key:
+            return key
+    return None
+
+
+filename = "..\\data\\d5_input.txt"
+# switchToTest()
+
+abs_file_path = os.path.join(os.path.dirname(__file__), filename)
+lines = open(abs_file_path, "r").readlines()
+lines = list(map(lambda x: x.strip(), lines))
+
+print(*lines, sep="\n")
+
+seeds = []
+mapRules = {}
+init(lines)
+
+print(seeds)
+util.printJson(mapRules)
+
+part1()
+part2()
