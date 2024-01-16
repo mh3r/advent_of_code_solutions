74eec42ea45b481aaa4bef84a0920e4a0ee45d7a
mh3r <96151246+mh3r@users.noreply.github.com>
2024-01-16 21:02:37 +1100

    Update day1.py

--- advent2023/src/day1.py
+++ advent2023/src/day1.py
@@ -14,58 +14,48 @@ def switchToTest():
 
 
 def part1(lines):
-    counts = []
+    answer = 0
     for line in lines:
-        print(line)
+        # can also use list comphrehension
+        # numbers = [c for c in line if c.isdigit()]
+        # same result when using regex
         numbers = re.findall(r"\d", line)
-        counts.append(int(numbers[0] + numbers[-1]))
+        answer += int(numbers[0] + numbers[-1])
 
-    print(counts)
-    ans = sum(counts)
-    print("total ", ans)
+    assert answer == 54990, f"answer is wrong {answer}"
+    print("total ", answer)
 
 
 def part2(lines):
-    counts = []
-    # better solution is ...
-    # line = line.replace("one", "o1e")
-    # line = line.replace("eight", "e8t")
-    
+    answer = 0
+
     for line in lines:
-        line = line.replace("oneight", "18")
-        line = line.replace("twone", "21")
-        line = line.replace("threeight", "38")
-        line = line.replace("fiveight", "58")
-        line = line.replace("sevenine", "79")
-        line = line.replace("eighthree", "83")
-        line = line.replace("eightwo", "82")
-        line = line.replace("nineight", "98")
-
-        line = line.replace("one", "1")
-        line = line.replace("two", "2")
-        line = line.replace("three", "3")
-        line = line.replace("four", "4")
-        line = line.replace("five", "5")
-        line = line.replace("six", "6")
-        line = line.replace("seven", "7")
-        line = line.replace("eight", "8")
-        line = line.replace("nine", "9")
-
+        line = (
+            line.replace("one", "o1e")
+            .replace("two", "t2o")
+            .replace("three", "t3e")
+            .replace("four", "f4r")
+            .replace("five", "f5e")
+            .replace("six", "s6x")
+            .replace("seven", "s7n")
+            .replace("eight", "e8t")
+            .replace("nine", "n9e")
+        )
         numbers = re.findall(r"\d", line)
-        counts.append(int(numbers[0] + numbers[-1]))
+        answer += int(numbers[0] + numbers[-1])
 
-    print(counts)
-    print("total ", sum(counts))
+    assert answer == 54473, f"answer is wrong {answer}"
+    print("total ", answer)
 
 
-filename = "..\\data\\d1_input.txt"#
+filename = "..\\data\\d1_input.txt"
 # switchToTest()
 
 abs_file_path = os.path.join(os.path.dirname(__file__), filename)
 lines = open(abs_file_path, "r").readlines()
 lines = list(map(lambda x: x.strip(), lines))
 
-print(*lines, sep="\n")
+# print(*lines, sep="\n")
 
 # part1(lines)
 part2(lines)
