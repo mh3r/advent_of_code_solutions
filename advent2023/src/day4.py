ff3c8c69395a6ea0e4aa5a8448b05622fe36d80c
mh3r <96151246+mh3r@users.noreply.github.com>
2024-01-16 23:54:03 +1100

    Update day4.py

--- advent2023/src/day4.py
+++ advent2023/src/day4.py
@@ -17,10 +17,8 @@ def init(lines):
     retval = []
     for line in lines:
         line = line.split(":")[1].strip()
-        splitted = line.split("|")
-
-        retval.append([splitted[0].strip().split(" "), splitted[1].strip().split(" ")])
-        pass
+        winners, owned = line.split("|")
+        retval.append([winners.split(), owned.split()])
     retval = lines if len(retval) == 0 else retval
     return retval
 
@@ -28,54 +26,48 @@ def init(lines):
 def part1(input):
     total = 0
     for game in input:
-        power = -1
-        for number in game[1]:
-            if number != "" and number in game[0]:
-                power += 1
-        if power > -1:
-            total += math.pow(2, power)
-        print(game)
-    print("answer", int(total))
-    pass
+        winners, owned = game
+        thePowa = len([number for number in owned if number in winners])
+        if thePowa:
+            total += math.pow(2, thePowa - 1)
+        # print(game)
 
-
-def part2(input):
-    pointsByGame = {}
-    counter = 1
-    for game in input:
-        power = 0
-        for number in game[1]:
-            if number != "" and number in game[0]:
-                power += 1
-        pointsByGame[str(counter)] = power
-        counter += 1
-
-    print("pointsByGame", pointsByGame)
-    total = 0
-
-    tallies = {}
-    for i in range(1, len(pointsByGame) + 1):
-        tallies[str(i)] = 1
-
-    for key in pointsByGame:
-        for i in range(int(key) + 1, int(key) + pointsByGame[key] + 1):
-            tallies[str(i)] += tallies[key]
-
-    print("tallies     ", tallies)
-    for key in tallies:
-        total += tallies[key]
+    total = int(total)
+    assert total == 21568, f"total is wrong {total}"
     print("answer", total)
     pass
 
 
+def part2(input):
+    total = 0
+    pointsByGame = {}
+    tallies = {}
+    for counter, game in enumerate(input):
+        winners, owned = game
+        power = len([number for number in owned if number in winners])
+        daCounter = str(counter + 1)
+        pointsByGame[daCounter] = power
+        tallies[daCounter] = 1
+
+    print("pointsByGame", pointsByGame)
+
+    for key in pointsByGame:
+        for i in range(int(key) + 1, int(key) + pointsByGame[key] + 1):
+            tallies[str(i)] += tallies[key]
+        total += tallies[key]
+
+    assert total == 11827296, f"total is wrong {total}"
+    print("answer", total)
+
+
 filename = "..\\data\\d4_input.txt"
-switchToTest()
+# switchToTest()
 
 abs_file_path = os.path.join(os.path.dirname(__file__), filename)
 lines = open(abs_file_path, "r").readlines()
 lines = list(map(lambda x: x.strip(), lines))
 
-print(*lines, sep="\n")
+# print(*lines, sep="\n")
 
 input = init(lines)
 
