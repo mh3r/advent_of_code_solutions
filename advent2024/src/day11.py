1d774db6e6ae08d628fde99f10153c69b81dcaf1
mh3r <96151246+mh3r@users.noreply.github.com>
2024-12-11 18:55:15 +1100

    had to look at others answers :(

--- advent2024/src/day11.py
+++ advent2024/src/day11.py
@@ -7,6 +7,7 @@ import json
 import os
 import types
 import math
+from functools import cache
 
 sys.path.append(os.getcwd() + "\py-util")
 import util
@@ -33,10 +34,7 @@ def part1():
     stoneList = list(map(int, stones.split(" ")))
 
     for stone in stoneList:
-        tmpStone = [stone]
-        for i in range(25):
-            tmpStone = blink(tmpStone)
-        answer += len(tmpStone)
+        answer += blinkRecur(stone, 25)
 
     # print(stoneList)
     # answer = len(stoneList)
@@ -50,13 +48,8 @@ def part2():
     stones = input[0].strip()
 
     stoneList = list(map(int, stones.split(" ")))
-
     for stone in stoneList:
-        tmpStone = [stone]
-        for i in range(75):
-            tmpStone = blink(tmpStone)
-        answer += len(tmpStone)
-        print(answer)
+        answer += blinkRecur(stone, 75)
 
     # print(stoneList)
     # answer = len(stoneList)
@@ -65,10 +58,34 @@ def part2():
     pass
 
 
+@cache
+def blinkRecur(stone, times):
+    if times == 0:
+        return 1
+
+    nextTime = times - 1
+    if stone == 0:
+        return blinkRecur(1, nextTime)
+
+    digits = int(math.log10(stone)) + 1
+    if digits % 2 == 0:
+        halfIndex = int(digits / 2)
+        firstHalf = first_n_digits(stone, halfIndex)
+        secondHalf = last_n_digits(stone, halfIndex)
+        return blinkRecur(firstHalf, nextTime) + blinkRecur(secondHalf, nextTime)
+
+    return blinkRecur(stone * 2024, nextTime)
+
+
 def blink(stones):
     retval = []
 
     for stone in stones:
+        stoneStr = str(stone)
+        if stoneStr in blinkMap:
+            retval.extend(blinkMap[stoneStr])
+            continue
+
         if stone == 0:
             retval.append(1)
             continue
@@ -76,11 +93,16 @@ def blink(stones):
         digits = int(math.log10(stone)) + 1
         if digits % 2 == 0:
             halfIndex = int(digits / 2)
-            retval.append(first_n_digits(stone, halfIndex))
-            retval.append(last_n_digits(stone, halfIndex))
+            firstHalf = first_n_digits(stone, halfIndex)
+            secondHalf = last_n_digits(stone, halfIndex)
+            retval.append(firstHalf)
+            retval.append(secondHalf)
+            blinkMap[stoneStr] = [firstHalf, secondHalf]
             continue
 
-        retval.append(2024 * stone)
+        value = 2024 * stone
+        retval.append(value)
+        blinkMap[stoneStr] = [value]
 
     return retval
 
@@ -102,7 +124,9 @@ lines = list(map(lambda x: x.strip(), lines))
 
 # print(*lines, sep="\n")
 
+blinkMap = {"0": [1]}
+
 input = init(lines)
 
-# part1()
+part1()
 part2()
