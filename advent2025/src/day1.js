9736e710fdc420ccfdac12f6707e5e2de59c80fa
mh3r <96151246+mh3r@users.noreply.github.com>
2025-12-06 15:58:39 +1100

    d1

--- advent2025/src/day1.js
+++ advent2025/src/day1.js
@@ -6,7 +6,21 @@ const dataDir = `${baseDir}\\data`;
 
 function part1(config) {
     let answer = 0;
-    const correctAnswer = undefined
+    const correctAnswer = 1141
+    let cursor = 50
+    for (let instruction of config.instructions) {
+        instruction = instruction % 100
+        cursor += instruction
+
+        if (cursor < 0) {
+            cursor += 100
+        } else if (cursor >= 100) {
+            cursor -= 100
+        }
+        if (cursor === 0) {
+            answer++
+        }
+    }
 
     console.log(`Part 1: ${answer}`)
     console.assert(correctAnswer === answer, `${answer} should have been ${correctAnswer}`);
@@ -14,15 +28,40 @@ function part1(config) {
 
 function part2(config) {
     let answer = 0;
-    const correctAnswer = undefined
+    const correctAnswer = 6634
+
+    let cursor = 50
+    for (let instruction of config.instructions) {
+        const startingCursor = cursor
+        const projection = cursor += instruction
+
+        if (projection >= 0 && projection < 100) {
+            cursor = projection
+            if (cursor === 0) {
+                answer++
+            }
+            continue
+        }
+
+
+        let rotations = Math.abs(Math.trunc(projection / 100))
+        // unsure why cursor doesnt work here ... it doesnt get changed 
+
+        if (startingCursor > 0 && projection < 0) {
+            rotations++
+        }
+        answer += rotations
+
+        cursor = (projection % 100 + 100) % 100
+    }
 
     console.log(`Part 2: ${answer}`)
     console.assert(correctAnswer === answer, `${answer} should have been ${correctAnswer}`);
 }
-
+ 
 function main() {
     let inputFile = `${dataDir}\\d1_input.txt`;
-    inputFile = `${baseDir}\\data\\test.txt`;
+    // inputFile = `${baseDir}\\data\\test.txt`;
 
     console.log("Input File: " + inputFile);
     const lines = tools.readFileFromLocal(inputFile).split(/\r?\n/).filter(x => x);
@@ -31,6 +70,14 @@ function main() {
     const config = {
         raw: lines
     }
+
+    const instructions = []
+    for (const line of lines) {
+        const modifier = (line[0] === 'L') ? -1 : 1
+        instructions.push(modifier * parseInt(line.slice(1)))
+
+    }
+    config.instructions = instructions
 
     part1({ ...config });
     part2({ ...config });
